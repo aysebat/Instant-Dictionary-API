63fb7c01da1e8991a7972e21e120a84211e0ba01
aysebat <aaysebatt@gmail.com>
2023-02-05 08:58:16 +0300

    UPdate the api class

--- api.py
+++ api.py
@@ -2,22 +2,24 @@ import justpy as jp
 import definition
 import json
 
+
 class Api:
     """Handles requests at /api?w=word
     """
+
     @classmethod
     def serve(cls, req):
         wp = jp.WebPage()
+        #getting teh word from the URL
         word = req.query_params.get('w')
 
         defined = definition.Definition(word).get()
 
         response = {
-            "word":word,
-            "definition":defined
+            "word": word,
+            "definition": defined
         }
-
+        #just pass the text what the request get
         wp.html = json.dumps(response)
         return wp
-
 

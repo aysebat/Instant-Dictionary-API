c0c8bdb59d6f3d16464e99b98ed07f025bd77fc5
aysebat <aaysebatt@gmail.com>
2023-02-08 20:16:41 +0300

    updating the margin

--- documentation.py
+++ documentation.py
@@ -17,7 +17,8 @@ class Doc:
 
         jp.Hr(a=mainDiv)
         jp.Div(a=mainDiv,
-               text="www.example.com/api?w=moon", #put your domain webpage
+               text="www.example.com/api?w=moon",
+               classes="mx-2"
                )
         jp.Hr(a=mainDiv)
         jp.Div(a=mainDiv,
@@ -27,5 +28,6 @@ class Doc:
                "To fuss over adoringly or with great affection.",
                "Deliberately show ones bare ass (usually to an audience, or at a place, where this is not expected or deemed appropriate).",
                "To be lost in phantasies or be carried away by some internal vision, having temorarily lost (part of) contact to reality."]}
-               """)
+               """,
+               classes="mx-2")
         return wp
